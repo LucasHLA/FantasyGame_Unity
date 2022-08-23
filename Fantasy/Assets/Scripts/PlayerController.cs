8bade08fba62fb05e2697b5df3c5349880828ec6
Lucas <LucashenriqueCNA@gmail.com>
2022-08-23 18:16:56 -0300

    Script Player Controller

--- Fantasy/Assets/Scripts/PlayerController.cs
+++ Fantasy/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+
+    [SerializeField] private int health;
+    [SerializeField] private float speed;
     
     void Start()
     {
