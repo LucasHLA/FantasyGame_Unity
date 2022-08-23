4df85e011f2bf6222cda16a9d8e8904579b943db
Lucas <LucashenriqueCNA@gmail.com>
2022-08-23 14:39:03 -0300

    Character and first script

---
+++ Fantasy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
