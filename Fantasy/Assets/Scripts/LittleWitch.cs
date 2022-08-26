d6a5d0b12ee717a61434f0ab15bcd19c9cdc4682
Lucas <LucashenriqueCNA@gmail.com>
2022-08-26 13:20:17 -0300

    Character movement

--- Fantasy/Assets/Scripts/LittleWitch.cs
+++ Fantasy/Assets/Scripts/LittleWitch.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LittleWitch : MonoBehaviour
+public class LittleWitch : PlayerController
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isShooting;
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
     }
+
+
 }
--- Fantasy/Assets/Scripts/PlayerController.cs
+++ Fantasy/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Animator anim;
+    protected Rigidbody2D rb;
+    protected Animator anim;
+    protected Transform tr;
+    protected Collider2D col2D;
+    [SerializeField] protected LayerMask ground;
+    protected enum State {idle, walking, jumping, falling}
+    [SerializeField] protected State state;  
 
     [SerializeField] private int health;
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
     
-    void Start()
+
+    protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        tr = GetComponent<Transform>();
+        col2D = GetComponent<Collider2D>();
+
     }
 
-    
-    void Update()
+    protected virtual void Update()
     {
-        
+        anim.SetInteger("state", (int)state);
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        state = State.walking;
+
+        if (horizontal > 0)
+        {
+            tr.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(horizontal < 0)
+        {
+            tr.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (Mathf.Abs(rb.velocity.x) <= 0)
+        {
+            state = State.idle;
+        }
+
+        if(Input.GetButtonDown("Jump") && col2D.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        state = State.jumping;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);   
     }
 }
+
+  
